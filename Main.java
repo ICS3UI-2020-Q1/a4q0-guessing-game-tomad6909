f9c277ea50e8c54ac7a51f772bf4a17a409d5821
tomad6909 <tomad6909@wrdsb.ca>
2020-10-03 20:57:05 +0000

    finished A4Q0

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
- *
- * @author 
+ * This program chooses a random number and the user needs to guess that number
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create the Scanner
+    Scanner input = new Scanner(System.in);
+    // create a random number generator
+    Random rand = new Random();
+
+    // get the random number to guess
+    int randInt = rand.nextInt(100) + 1;
+
+    // declare the guess variable
+    int guess;
+
+    // create a loop to keep guessing
+    do {
+      System.out.println("Please enter your guess between 1 and 100");
+      guess = input.nextInt();
+
+      if (guess == randInt){
+        System.out.println("You are correct! Good job!");
+      } else if ( guess > randInt){
+      System.out.println("You are too high. Try again.!");
+    } else {
+      System.out.println("You are too low. Try again!");
+    }
+    }while (guess != randInt);
   }
 }
